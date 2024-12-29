a8890729e96bcdbef2a27d4ba70bfe0c5939e397
Rocky <54959334+RJO-Jorn@users.noreply.github.com>
2024-12-29 16:10:18 +0100

    Github Transfer
    From Local Development to Github Transfer

---
+++ head.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="utf-8">
+    <title>RJO-Web</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="icon.png">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <link rel="stylesheet" href="./css/main.css">
+    
+    <!-- Include your image file -->
+    <link rel="icon" type="image/ico" href="./img/favicon.ico">
+    <script src="https://kit.fontawesome.com/2a834fb06b.js" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+</head>
